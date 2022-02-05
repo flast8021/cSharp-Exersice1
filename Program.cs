20b085de08e597bf940e39479e88d10fe5f28c03
Usman Zia <24087@student.dorset-college.ie>
2022-02-05 12:13:58 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+/*
+         * Build a simple calculator. 
+         * Read in two integers from the user 
+         * and add them together. 
+         * subtract
+         * multiply
+         * division(display quotient and remainder separately)
+         * Print the results in a nice output statement.
+         * 
+         * 
+         * */
+namespace exersice1
+{
+    //------------------------------------------------------------
+    //Title:-- UsmanZia24087_Exerseice#1
+    //Description:--This Programe contains all 3 Solutions of Questions asked in Exersice#1.
+    //------------------------------------------------------------//
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Calling Q#1 of Exersice#1
+            calculator calc = new calculator();
+            calc.simpleCalculator();
+
+            //Calling Q#2 of Exersice#1
+            carpetingPrice carpet = new carpetingPrice();
+            carpet.Carpeting_Price();
+
+            //Calling Q#3 of Exersice#1
+            minutesToHours timeCalc = new minutesToHours();
+            timeCalc.minToHour();
+        }
+    }
+}
---
+++ minutesToHours.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exersice1
+{
+    /*
+    Write a program that asks the user to enter a total amount of time in minutes only.
+    Print to the console the same amount of time in hours and minutes.*/
+    public class minutesToHours
+    {
+        public void minToHour()
+        {
+            Console.WriteLine("Exersice#1\tQuestion#3\nwelcome to Minutes to Hour calculator");
+            //grtting user input
+            Console.WriteLine("Please Enter total amount of time in minutes:");
+            int totalTime = int.Parse(Console.ReadLine());
+            //variables to calculte time in hourly format
+            int totalHours = totalTime / 60;
+            int totalMins = totalTime % 60;
+            //printing on console
+            Console.WriteLine($"Time in hourly format is: {totalHours} hours and {totalMins} mins.");
+            Console.WriteLine("------------------End----------------------");
+        }
+    }
+}
