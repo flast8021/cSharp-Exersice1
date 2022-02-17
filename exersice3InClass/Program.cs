584e89fd1c6c0ef155a0fec85a08c8e99c1eacc1
Usman Zia <88999305+flast8021@users.noreply.github.com>
2022-02-17 16:07:24 +0000

    Add files via upload
    Question 1 just missing one part and Question 3 is incomplete with this commit.

---
+++ exersice3InClass/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace exersice_3inClass
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+//Answer 1
+            Console.WriteLine("-----Answer#1-----");
+            deliveryService parcel = new deliveryService();
+            parcel.data();
+//Answer 2
+            Console.WriteLine("-----End of Answer#1-----");
+            Console.WriteLine("---------------------------------------------------");
+            Console.WriteLine("-----Answer#2-----");
+            studentGrading grades = new studentGrading("", 0.0);
+
+            Console.WriteLine("Please enter total number of studnts in class: ");
+            int totalStudents = Convert.ToInt32(Console.ReadLine());
+
+            int i;
+            double maxGrades = 0.0;
+            double avg = 0;
+            double achievedGrades = 0.0;
+            double totalObtained = 0.0;
+            for (i = 1; i <= totalStudents; ++i)
+            {
+                Console.WriteLine($"Please Enter Student# {i} Name: ");
+                grades.name = Console.ReadLine();
+
+                Console.WriteLine($"Plese enter {grades.name}'s Grades: ");
+                grades.grades = Convert.ToDouble(Console.ReadLine());
+
+
+                grades.studentList.Add(new studentGrading(grades.name, grades.grades) { });
+                if (i == 0)
+                {
+                    maxGrades = grades.grades;
+                    achievedGrades += grades.grades;
+                }
+                else
+                {
+                    if (grades.grades > maxGrades)
+                    {
+                        maxGrades = grades.grades;
+                        achievedGrades += grades.grades;
+                    }
+                }
+                avg = achievedGrades / i;
+            }
+            Console.WriteLine("The largest grades in class are {0:N}", maxGrades);
+            Console.WriteLine("The average grades in class are {0:N}", avg);
+
+
+            Console.WriteLine("-----End of Answer#2-----");
+
+
+
+
+//Answer 3
+            Console.WriteLine("---------------------------------------------------");
+            Console.WriteLine("-----Answer#3-----");
+
+            letterGuessingGame letterGuess = new letterGuessingGame();
+
+            for (int i = 0; i < 9; i++)
+            {
+                letterGuess.userGuess();
+            }
+
+
+            Console.WriteLine("-----End of Answer#3-----");
+            Console.WriteLine("---------------------------------------------------");
+            Console.WriteLine("-----------------------------------------");
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine("--------------------------");
+            Console.WriteLine("-----------------");
+            Console.WriteLine("-------");
+            Console.WriteLine("--");
+        }
+
+    }
+
+}
---
+++ exersice3InClass/deliveryService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exersice_3inClass
+{
+    public class deliveryService
+    {
+        /*Write a program for a package delivery service. The program contains an array that holds the
+        ten zip codes to which the company delivers packages. Prompt a user to enter a zip code and
+        display a message indicating whether the zip code is one to which the company delivers*/
+
+        public void data()
+        {
+            Console.WriteLine("Please enter Zip Code to get package details:");
+            string input = Console.ReadLine();
+
+            bool zipEntry = false;
+            
+            string[] zipCodes = {"A001","A002","A003","A004","A005","A006","A007","A008","A009","A010"};
+
+            for (int i = 0; i < zipCodes.Length && !zipEntry; ++i)    
+            {
+                if (input == zipCodes[i])
+                {
+                    Console.WriteLine($"Zip Code: {0}, accepted. Package not dispatched yet.");
+                    zipEntry = true;
+                    break;
+                }
+            }
+            if(!zipEntry)
+            {
+                Console.WriteLine($"Sorry! {input} Not found...");
+                //Console.ReadLine();
+                return;
+            }
+        }
+    }
+}
